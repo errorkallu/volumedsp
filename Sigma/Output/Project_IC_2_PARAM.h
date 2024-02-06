d04d1cb00cd966d228e5feb46fe712477e8fa248
errorkallu <41589543+errorkallu@users.noreply.github.com>
2024-02-05 19:53:13 -0800

    Add files via upload
    sigma

---
+++ Sigma/Output/Project_IC_2_PARAM.h
@@ -0,0 +1,23 @@
+/*
+ * File:           C:\Users\skhome_mediapc\OneDrive\Documents\Arduino\Wi-Fi-Audio-Control-master\Wi-Fi-Audio-Control-Test\Sigma\Output\Project_IC_2_PARAM.h
+ *
+ * Created:        Monday, February 5, 2024 4:21:27 PM
+ * Description:    Project:IC 2 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2024 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __PROJECT_IC_2_PARAM_H__
+#define __PROJECT_IC_2_PARAM_H__
+
+
+#endif
